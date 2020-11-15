f49e57c5e3c9016e0cf8a3064a5c1b28dd34fdaa
MrChuckomo <alexander.kettler91@gmail.com>
2020-11-15 22:23:27 +0100

    Update __init__.py
    # add enums for media and entity

--- podsearch/__init__.py
+++ podsearch/__init__.py
@@ -7,23 +7,45 @@ Version      : v0.1.0
 Creation Date: 11-Nov-2020
 """
 
-class Config(object):
-    """
-    docstring
-    """
-    pass
+from enum import Enum
 
+
+# ---------------------------------------------------------------------------------------------------------------------
 
 SEARCH_URL = 'https://itunes.apple.com/search?'
-MEDIA = [
-    'movie', 
-    'podcast', 
-    'music', 
-    'musicVideo', 
-    'audiobook', 
-    'shortFilm', 
-    'tvShow', 
-    'software', 
-    'ebook', 
-    'all'
-]
+
+
+class Media(Enum):
+    MOVIE = 'movie'
+    PODCAST = 'podcast'
+    MUSIC = 'music'
+    MUSICVIDEO = 'musicVideo'
+    AUDIOBOOK = 'audiobook'
+    SHORTFILM = 'shortFilm'
+    TVSHOW = 'tvShow'
+    SOFTWARE = 'software'
+    EBOOK = 'ebook'
+    ALL = 'all'
+
+
+class Entity():
+
+    class Movie(Enum):
+        MOVIEARTIST = 'movieArtist'
+        MOVIE = 'movie'
+
+    class Podcast(Enum):
+        PODCASTAUTHOR = 'podcastAuthor'
+        PODCAST = 'podcast'
+
+# movie	movieArtist, movie
+# podcast	podcastAuthor, podcast
+# music	musicArtist, musicTrack, album, musicVideo, mix, song
+# Please note that “musicTrack” can include both songs and music videos in the results
+# musicVideo	musicArtist, musicVideo
+# audiobook	audiobookAuthor, audiobook
+# shortFilm	shortFilmArtist, shortFilm
+# tvShow	tvEpisode, tvSeason
+# software	software, iPadSoftware, macSoftware
+# ebook	ebook
+# all	movie, album, allArtist, podcast, musicVideo, mix, audiobook, tvSeason, allTrack
